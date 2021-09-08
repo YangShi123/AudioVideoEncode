15dd8c005a1af372d1fb3da06302d2b176f86e9c
shiyang <shiyawn@163.com>
2021-09-08 15:53:27 +0800

    Initial Commit

---
+++ AudioVideoEncode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AudioVideoEncode
+//
+//  Created by Rex on 2021/9/8.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
